235b78a8a717fa98a36aaf22c1a3f3f99cbbdd70
Zhao Qiang <980689346@qq.com>
2021-10-30 17:11:11 +0800

    '10/30'

--- info/__init__.py
+++ info/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
@@ -14,6 +17,8 @@ redis_store = None
 def create_app(config_name):
     app = Flask(__name__)
 
+    # 调用日志方法 记录软件运行信息
+    log_file()
     #  加载配置类
     config = config_dict.get(config_name)
     app.config.from_object(config)
@@ -36,3 +41,17 @@ def create_app(config_name):
     app.register_blueprint(index_blue)
 
     return app
+
+
+def log_file():
+    # 设置日志的记录等级
+    # DEBUG = 10  INFO = 20 WARNING = 30 ERROR = 40
+    logging.basicConfig(level=logging.DEBUG)  # 调试debug级
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 * 100, backupCount=10)
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
