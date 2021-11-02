78b07973dfa39bbc039c8afa5cd8fbfa87f200c1
Zhao Qiang <980689346@qq.com>
2021-11-02 18:18:07 +0800

    11/2

--- info/__init__.py
+++ info/__init__.py
@@ -10,6 +10,7 @@ from redis import StrictRedis
 from config import config_dict
 
 #  定义全局变量
+
 from info.utils.commons import hot_news_filter
 
 redis_store = None
@@ -46,11 +47,14 @@ def create_app(config_name):
     # 将首页蓝图注册到app中
     from info.modules.index import index_blue
     from info.modules.passport import passport_blue
+    from info.modules.news import news_blue
     app.register_blueprint(index_blue)
     app.register_blueprint(passport_blue)
+    app.register_blueprint(news_blue)
 
     #  将函数添加到系统默认的过滤器列表 第一个参数是函数 第二个是过滤器的名字 这样的话就是说找这个过滤器 执行这个函数 参数就是|这个符合前面的东西
     app.add_template_filter(hot_news_filter, "my_filter")
+
 
     #  执行请求后钩子 将csrf放到session中
     @app.after_request
---
+++ info/modules/news/views.py
@@ -0,0 +1,53 @@
+from flask import render_template, jsonify, current_app, abort, session
+from ...models import News, User
+from ...utils.response_code import RET
+from . import news_blue
+
+
+# 9.新闻详情展示(新闻)
+# 请求路径: /news/<int:news_id>
+# 请求方式: GET
+# 请求参数:news_id
+# 返回值: detail.html页面, 用户,新闻data字典数据
+#  <int:news_id>这个和django一样啊 路由这样写动态类型 对应的视图函数还可以接收到这个news_id
+@news_blue.route('/<int:news_id>')
+def news_detail(news_id):
+    #  根据新闻编号查询新闻对象
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="新闻获取失败")
+
+    # 用户id在session中 只要一登录就可以取session 就这个意思 所以和index里面一样的写法
+    user_id = session.get("user_id")
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+    #  这个地方是获取前十条热门新闻 因为这个地方 不需要什么 直接就可以获取啊 就点赞最多的呗
+    #  多说一句 其实当用户点赞的时候 我们会添加对应的clicks的值 那么这个地方查的时候是不是会更新
+    try:
+        click_news = News.query.order_by(News.clicks.desc()).limit(10).all()
+    except Exception as e:
+        return jsonify(errno=RET.DBERR, errmsg="新闻获取失败")
+
+    news_list = []
+    for item in click_news:
+        news_list.append(item.to_dict())
+
+    if not news:
+        abort(404)
+    # 如果出错返回404 abort()函数的作用
+    # 可以让开发者在检测到web访问错误时，立即将错误信息返回回去，返回的错误码必须是已知http协议中的错误码
+
+    #  携带数据，渲染页面
+    data = {
+        "news_info": news.to_dict(),
+        "news_list": news_list,
+        "user_info": user.to_dict() if user else ""
+    }
+    return render_template("news/detail.html", data=data)
