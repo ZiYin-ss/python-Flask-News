2915dfed8e5ad46544ae00661b0c22ed0858ca42
Zhao Qiang <980689346@qq.com>
2021-11-03 22:00:31 +0800

    11/3

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,7 +1,6 @@
 from flask import render_template, g, redirect, request, jsonify
 
 from . import profile_blue
-from ... import db
 from ...utils.commons import user_login_data
 from ...utils.response_code import RET
 
@@ -48,3 +47,55 @@ def base_info():
     g.user.gender = gender
     # 7.返回响应
     return jsonify(errno=RET.OK, errmsg="操作成功")
+
+
+# 请求路径: /user/pic_info
+# 请求方式:GET,POST
+# 请求参数:无, POST有参数,avatar
+# 返回值:GET请求: user_pci_info.html页面,data字典数据, POST请求: errno, errmsg,avatar_url
+@profile_blue.route("/pic_info", methods=['GET', 'POST'])
+@user_login_data
+def pic_info():
+    # 1.判断请求方式, 如果是get请求
+    if request.method == "GET":
+        return render_template("news/user_pic_info.html", user_info=g.user.to_dict())
+    # 2.携带用户的数据, 渲染页面
+    pass
+    # 3.如果是post请求
+    # 4.获取参数
+    # 5.校验参数, 为空校验
+    # 6. 上传图像, 判断图片是否上传成功
+    # 7.将图片设置到用户对象
+    # 8.返回响应
+
+
+# 获取/设置用户密码
+# 请求路径: /user/pass_info
+# 请求方式:GET,POST
+# 请求参数:GET无, POST有参数,old_password, new_password
+# 返回值:GET请求: user_pass_info.html页面,data字典数据, POST请求: errno, errmsg
+@profile_blue.route("/pass_info", methods=['GET', 'POST'])
+@user_login_data
+def pass_info():
+    #   1. 判断请求方式,如果是get请求
+    #   2. 直接渲染页面
+    if request.method == "GET":
+        return render_template("news/user_pass_info.html", user_info=g.user.to_dict())
+
+    #   3. 如果是post请求,获取参数
+    old_password = request.json.get("old_password")
+    new_password = request.json.get("new_password")
+
+    #   4. 校验参数,为空校验
+    if not all([old_password, new_password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不全")
+
+    #   5. 判断老密码是否正确
+    if not g.user.check_passowrd(old_password):
+        return jsonify(errno=RET.DATAERR, errmsg="密码错误")
+
+    #   6. 设置新密码
+    g.user.password = new_password
+
+    #   7. 返回响应
+    return jsonify(errno=RET.OK, errmsg="修改成功")
