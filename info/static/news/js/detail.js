6de071236aa81196e5a5b21ed821f7899eb9156e
Zhao Qiang <980689346@qq.com>
2021-11-03 14:02:07 +0800

    11/3

--- info/static/news/js/detail.js
+++ info/static/news/js/detail.js
@@ -229,7 +229,7 @@ $(function(){
         // 评论回复
         if(sHandler.indexOf('reply_sub')>=0)
         {
-            /*
+
             var $this = $(this)
             var news_id = $this.parent().attr('data-newsid')
             var parent_id = $this.parent().attr('data-commentid')
@@ -299,7 +299,6 @@ $(function(){
                     }
                 }
             })
-            */
         }
     })
 
